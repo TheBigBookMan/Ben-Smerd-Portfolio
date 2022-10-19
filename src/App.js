24849563533503384df8949b4c6b59e39f85a1fe
Ben Smerd <bsmerd@adam.com.au>
2022-10-19 13:04:50 +1030

    imported components to App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import './styles/App.css';
+import "./styles/App.css";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    
+    <main>
+      <Header />
+      {/* <Main />
+      <Footer /> */}
+    </main>
   );
 }
 
