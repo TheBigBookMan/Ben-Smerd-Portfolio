9f4501e670fa7a954b3c3ba622e84e9c63a7e2c3
Ben Smerd <bsmerd@adam.com.au>
2022-10-19 13:05:13 +1030

    created Header and added in hero image+title with styling in css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,17 @@
 // conatins the title of my name with the navigation bar in it
+import React from "react";
+import "../styles/Header.css";
+import Me from "../images/Me.jpg";
+
+const Header = () => {
+  return (
+    <header id="header-container">
+      <div className="hero-container">
+        <img src={Me} alt="self-portrait" id="hero-portrait" />
+        <h1 id="hero-name">Ben Smerd</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
