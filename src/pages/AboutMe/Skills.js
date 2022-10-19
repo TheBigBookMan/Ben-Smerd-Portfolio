5c92b5789b4ea29e36bfe7223c84be5f30b5ce32
Ben Smerd <bsmerd@adam.com.au>
2022-10-19 22:18:02 +1030

    skills in about me added

--- src/pages/AboutMe/Skills.js
+++ src/pages/AboutMe/Skills.js
@@ -8,13 +8,42 @@ const skillsStyles = {
     border: "solid rgb(46, 94, 176) 5px",
     borderRadius: "50px",
     padding: "20px",
+    lineHeight: "30px",
+    backgroundColor: "#fee4ff",
+  },
+  skillsTitle: {
+    color: "rgb(46, 94, 176)",
+  },
+  skillsName: {
+    fontStyle: "italic",
+    color: "#10c6ef",
+  },
+  skillsText: {
+    letterSpacing: "1px",
   },
 };
 
 const Skills = () => {
   return (
     <div style={skillsStyles.skillsContainer}>
-      <h2>Skills</h2>
+      <h2 style={skillsStyles.skillsTitle}>Skills</h2>
+      <h3 style={skillsStyles.skillsName}>Technologies</h3>
+      <p style={skillsStyles.skillsText}>
+        MERN stack (MongoDB, ExpressJS, ReactJS, NodeJS), ES6 Javascript,
+        JQuery, MySQL, TailwindCSS, Bootstrap, GitHub, GitLab, Git, Heroku and
+        more.
+      </p>
+      <h3 style={skillsStyles.skillsName}>Concepts</h3>
+      <p style={skillsStyles.skillsText}>
+        Object Oriented Programming, Progressive Web Apps, Responsive Design,
+        ORM/ODM, CRUD, MVC and others.
+      </p>
+      <h3 style={skillsStyles.skillsName}>Soft Skills</h3>
+      <p style={skillsStyles.skillsText}>
+        Problem solving skills, emotional intelligence, leadership skills,
+        strong work ethic, ability to work independently and collaboratively,
+        communication skills, adaptability and curiosity to learn.
+      </p>
     </div>
   );
 };
