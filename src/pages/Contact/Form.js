e6e8f9bd8f59c0af5efb186418bc8494fd6d5532
Ben Smerd <bsmerd@adam.com.au>
2022-10-20 11:11:28 +1030

    added in contact Form html and styling

--- src/pages/Contact/Form.js
+++ src/pages/Contact/Form.js
@@ -1,6 +1,30 @@
 import React from "react";
 
-const formStyles = {};
+const formStyles = {
+  formContainer: {
+    display: "flex",
+    flexDirection: "column",
+    gap: "10px",
+    marginLeft: "20px",
+  },
+  inputBox: {
+    height: "30px",
+    fontSize: "1.2rem",
+    paddingLeft: "5px",
+  },
+  messageBox: {
+    fontSize: "1.2rem",
+    paddingLeft: "5px",
+  },
+  submitBox: {
+    height: "60px",
+    width: "150px",
+    borderRadius: "30px",
+  },
+  formTitles: {
+    color: "rgb(46, 94, 176)",
+  },
+};
 
 // name, email and message textbox
 // could put in an automatic sender to my email when click submit button
@@ -8,14 +32,47 @@ const formStyles = {};
 
 const Form = () => {
   return (
-    <form>
-      <input type="text" placeholder="Your Name" name="name" required></input>
-      <input type="text" placeholder="Your Email" name="email" required></input>
-      <textarea
-        placeholder="Enter your message"
-        name="message"
-        required
-      ></textarea>
+    <form style={formStyles.formContainer}>
+      <label htmlFor="name">
+        <h2 style={formStyles.formTitles}>Name:</h2>
+        <br></br>
+        <input
+          style={formStyles.inputBox}
+          type="text"
+          placeholder="Your Name"
+          name="name"
+          required
+        ></input>
+      </label>
+
+      <label htmlFor="email">
+        <h2 style={formStyles.formTitles}>Email:</h2>
+        <br></br>
+        <input
+          style={formStyles.inputBox}
+          type="text"
+          placeholder="Your Email"
+          name="email"
+          required
+        ></input>
+      </label>
+
+      <label htmlFor="message">
+        <h2 style={formStyles.formTitles}>Message:</h2>
+        <br></br>
+        <textarea
+          style={formStyles.messageBox}
+          placeholder="Enter your message"
+          name="message"
+          required
+          rows="6"
+          cols="30"
+        ></textarea>
+      </label>
+
+      <button style={formStyles.submitBox} type="submit" name="submit">
+        <h2 style={formStyles.formTitles}>Submit</h2>
+      </button>
     </form>
   );
 };
